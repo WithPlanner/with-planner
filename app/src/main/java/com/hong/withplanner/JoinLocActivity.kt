2b13713ed7dc3f2c62200a031bee23f74144f2db
hongggs <hongseoda@gmail.com>
2022-07-10 14:00:40 +0900

    modify: 줄 변경

--- app/src/main/java/com/hong/withplanner/JoinLocActivity.kt
+++ app/src/main/java/com/hong/withplanner/JoinLocActivity.kt
@@ -7,5 +7,6 @@ class JoinLocActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_loc)
+
     }
 }
