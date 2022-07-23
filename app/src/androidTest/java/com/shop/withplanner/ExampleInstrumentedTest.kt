e089f7a9a33aa4f6ce36027184aa1d4e815afc71
DESKTOP-PDO6139\win <leaf9793@naver.com>
2022-07-23 11:20:21 +0900

    rename: 패키지명 안바뀐 부분 변경

--- app/src/androidTest/java/com/shop/withplanner/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/shop/withplanner/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.hong.withplanner
+package com.shop.withplanner
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hong.withplanner", appContext.packageName)
+        assertEquals("com.shop.withplanner", appContext.packageName)
     }
 }
--- app/src/main/java/com/shop/withplanner/LoginActivity.kt
+++ app/src/main/java/com/shop/withplanner/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 import androidx.databinding.DataBindingUtil
 import com.shop.withplanner.databinding.ActivityLoginBinding
