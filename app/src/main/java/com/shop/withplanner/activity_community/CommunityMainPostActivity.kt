75b4055a983e641ff6ab0434de08d750ac921ccb
hongggs <hongseoda@gmail.com>
2022-08-09 10:15:39 +0900

    feat: 상세 게시물 커뮤니티 값 알맞은 곳에 할당

--- app/src/main/java/com/shop/withplanner/activity_community/CommunityMainPostActivity.kt
+++ app/src/main/java/com/shop/withplanner/activity_community/CommunityMainPostActivity.kt
@@ -1,13 +1,16 @@
 package com.shop.withplanner.activity_community
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.shop.withplanner.R
 import com.shop.withplanner.databinding.ActivityCommunityMainPostBinding
 import com.shop.withplanner.dto.CommunityPostMain
@@ -15,6 +18,7 @@ import com.shop.withplanner.recyler_view.PostModel
 import com.shop.withplanner.recyler_view.PostsAdapter
 import com.shop.withplanner.retrofit.RetrofitService
 import com.shop.withplanner.shared_preferences.SharedManager
+import com.shop.withplanner.util.Category
 import retrofit2.Call
 import retrofit2.Response
 
@@ -26,6 +30,7 @@ class CommunityMainPostActivity : AppCompatActivity() {
     private val sharedManager: SharedManager by lazy { SharedManager(this) }
 
     private lateinit var communityName : String
+    private lateinit var communityImg : String
     private lateinit var createdAt : String
     private lateinit var introduce : String
     private lateinit var category : String
@@ -33,6 +38,7 @@ class CommunityMainPostActivity : AppCompatActivity() {
     private var currentCount : Int = 0
     private lateinit var days : List<String>
     private lateinit var time : String
+    var context : Context = this
 
 
         override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,13 +59,52 @@ class CommunityMainPostActivity : AppCompatActivity() {
                         Log.d("PostCommunityMain", "onResponse 성공: " + result?.toString());
                         communityId = result.communityId.toLong()
                         communityName = result.name
+                        communityImg = result.communityImg
                         createdAt = result.createdAt
                         introduce = result.introduce
-                        category = result.category
+                        category = Category.category2string(result.category)
                         headCount = result.headCount
                         currentCount = result.currentCount
                         days = result.days
                         time = result.time
+
+
+                        Glide.with(context)
+                            .load(communityImg)
+                            .into(binding.mainImg)
+                        binding.titleTextView.text = communityName
+                        binding.validTextView.text = category
+                        binding.teamCountTextView.text = "$currentCount / $headCount"
+                        binding.dateTextView.text = createdAt
+                        for (day in days) {
+                            when (day) {
+                                "월" -> {
+                                    binding.mon.visibility = View.VISIBLE
+                                }
+                                "화" -> {
+                                    binding.tue.visibility = View.VISIBLE
+                                }
+                                "수" -> {
+                                    binding.wed.visibility = View.VISIBLE
+                                }
+                                "목" -> {
+                                    binding.thu.visibility = View.VISIBLE
+                                }
+                                "금" -> {
+                                    binding.fri.visibility = View.VISIBLE
+                                }
+                                "토" -> {
+                                    binding.sat.visibility = View.VISIBLE
+                                }
+                                "일" -> {
+                                    binding.sun.visibility = View.VISIBLE
+                                }
+                            }
+                        }
+                        binding.timeTextView.text = time
+                        binding.contentTextView.text = introduce
+                        binding.contentDateTextView.text = createdAt
+
                     } else {
                         Log.d("PostCommunityMain", "onResponse 실패: ");
                     }
@@ -70,6 +115,7 @@ class CommunityMainPostActivity : AppCompatActivity() {
                 }
 
             } )
+
 
 
         binding.backBtn.setOnClickListener {
@@ -112,6 +158,10 @@ class CommunityMainPostActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         super.onBackPressed()
+    }
+
+    fun makeMain() {
+
     }
 
 
